7632d3352f32226a8964b7b6402125c4062b338a
Eduardo Rolim <ens.rolim@gmail.com>
2022-07-16 22:37:00 +0000

    Update git-prompt.sh

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,4 +1,5 @@
 # bash/zsh git prompt support.
+# https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 #
 # Copyright (C) 2006,2007 Shawn O. Pearce <spearce@spearce.org>
 # Distributed under the GNU General Public License, version 2.0.
